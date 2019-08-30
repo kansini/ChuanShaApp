8fbf5a074e958f303a44b7753ca46114152b4a2a
flood <kansini@gmail.com>
2019-08-30 14:41:50 +0800

    init

--- src/views/List.vue
+++ src/views/List.vue
@@ -4,7 +4,11 @@
 			<span>{{title}}</span>
 		</div>
 		<div class="list-content">
-
+			<div class="list-group" v-for="(item,index) in NPCMemberList" :key="item.id">
+				<div class="list-item">{{item.id}}</div>
+				<div class="list-item">{{item.name}}</div>
+				<div class="list-item">{{item.position}}</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -14,7 +18,22 @@
 		name: "List",
 		data() {
 			return {
-				title: '人大代表工作室'
+				title: '人大代表工作室',
+				NPCMemberList:[{
+					"id":"001",
+					"name":"马桂英",
+					"position":"新吉村民委员会村委委员"
+				},
+				{
+					"id":"002",
+					"name":"马桂英",
+					"position":"新吉村民委员会村委委员"
+				},
+				{
+					"id":"003",
+					"name":"马桂英",
+					"position":"新吉村民委员会村委委员"
+				}]
 			}
 		},
 	}
@@ -32,19 +51,64 @@
 			font-size: calc(56rem / 96);
 			height: calc(88rem / 96);
 			line-height: calc(88rem / 96);
-			//background: #fff;
 			font-family: "xiaokai";
 			font-weight: bolder;
-			color: rgba(173, 0, 10, 1);
+			color: #d22000;
+			span{
+				position: relative;
+				&::before{
+					position: absolute;
+					content: '';
+					left: calc(-32rem / 96);
+					top: calc(20rem / 96);
+					width: calc(24rem / 96);
+					height: calc(24rem / 96);
+					border-radius: 2px;
+					background: rgba(173, 0, 10, 1);
+					transform: rotate(45deg);
+				}
+				&::after{
+					position: absolute;
+					content: '';
+					right: calc(-32rem / 96);
+					top: calc(20rem / 96);
+					width: calc(24rem / 96);
+					height: calc(24rem / 96);
+					border-radius: 2px;
+					background: rgba(173, 0, 10, 1);
+					transform: rotate(45deg);
+				}
+			}
 		}
 
 		.list-content {
+			padding: calc(24rem / 96);
+			font-family: "kaiti";
 			width: calc(996rem / 96);
 			height: calc(600rem / 96);
-			background: rgba(255, 0, 0, .7);
+			background: rgba(211, 16, 26, .7);
 			border: 4px solid #fff;
 			border-radius: calc(8rem / 96);
 			box-sizing: border-box;
+			font-size: calc(22rem / 96);
+			color: #fff;
+			.list-group{
+				display: flex;
+				justify-content: flex-start;
+				.list-item{
+					height: calc(48rem / 96);
+					line-height: calc(48rem / 96);
+					&:nth-child(1){
+						width: 10%;
+					}
+					&:nth-child(2){
+						width: 20%;
+					}
+					&:nth-last-child(1){
+						width: 60%;
+					}
+				}
+			}
 		}
 	}
 </style>
